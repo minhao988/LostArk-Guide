70ce03dc2daee13f4003389ca6446dc9f38eea82
minhao988 <59048866+minhao988@users.noreply.github.com>
2026-01-12 10:52:14 +0800

    Update 0.02

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ const sections = document.querySelectorAll("section");
 const links = document.querySelectorAll("#toc a");
 
 window.addEventListener("scroll", () => {
-  let currentId = "";
+    let current = "";
+    sections.forEach(sec => {
+        if (window.scrollY >= sec.offsetTop - 120) current = sec.id;
+    });
 
-  sections.forEach(section => {
-    const offset = section.offsetTop - 120;
-    if (window.scrollY >= offset) {
-      currentId = section.id;
-    }
-  });
-
-  links.forEach(link => {
-    link.classList.remove("active");
-    if (link.getAttribute("href") === "#" + currentId) {
-      link.classList.add("active");
-    }
-  });
+    links.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
 });
